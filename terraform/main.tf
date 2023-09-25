087bf3ea3b5b2162a03f187f520e81b9d4285f76
Tim <timothyqhtran@gmail.com>
2023-09-26 07:55:11 +1000

    main.tf push

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,14 @@
 resource "google_compute_instance" "vm" {
+    for_each = var.instances
+
     project = var.project_id
-    name = var.instance_name
-    description = var.description
-    machine_type = var.machine_type
-    zone = var.zone
-    hostname = var.hostname
+    name = each.key
+    description = each.value.description
+    machine_type = each.value.machine_type
+    zone = each.value.zone
+    hostname = "${each.key}-${each.value.zone}.asx.com.au"
 	
+    //Checks if the the input "service_account" is not equal to an empty string, if not empty generate a service_account block with content email and scope
     dynamic service_account {
         for_each = var.service_account != "" ? [1] : []
         content {
